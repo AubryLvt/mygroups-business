106b435e76d7fe730f2ae07c59070f18f5ef8e41
Aubry Thomas LOUVET <129608889+AubryLvt@users.noreply.github.com>
2024-04-06 15:20:36 +0200

    Initial commit

---
+++ src/main/java/app/mygroupsbusiness/MygroupsBusinessLayerApplication.java
@@ -0,0 +1,13 @@
+package app.mygroupsbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygroupsBusinessLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygroupsBusinessLayerApplication.class, args);
+	}
+
+}
